551447ccfe0c8f89ed91156c72ce66bbf62a5ccb
jcamiel <jeanchristophe.amiel@gmail.com>
2020-06-29 09:44:48 +0200

    Update version 0.99.9 -> 0.99.10.

--- Formula/hurl.rb
+++ Formula/hurl.rb
@@ -1,15 +1,15 @@
 class Hurl < Formula
   desc "Perform and tests HTTP session."
   homepage "https://hurl.dev"
-  url "https://github.com/fabricereix/hurl/releases/download/0.99.9/hurl-0.99.9-x86_64-osx.tar.gz"
-  sha256 "922900e7d85dfcd6f253323780ce5726a8d10e491248d9e038703bf32fde2dcd"
+  url "https://github.com/fabricereix/hurl/releases/download/0.99.10/hurl-0.99.10-x86_64-osx.tar.gz"
+  sha256 "f393ffa8c171da0f1486d5e45de9f4d30b92cc3db1d64360a1723d1071d2e6a7"
 
 
   def install
     bin.install "hurl"
     bin.install "hurlfmt"
 
-    #man1.install "hurl.1"
+    man1.install "hurl.1.gz"
     #man1.install "hurlfmt.1"
   end
 
