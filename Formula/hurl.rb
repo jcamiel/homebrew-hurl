0dcfc1ff091dbe907a90ca4f3215453055d933e5
jcamiel <jeanchristophe.amiel@gmail.com>
2021-03-05 17:39:24 +0100

    Update Hurl version 1.2.0

--- Formula/hurl.rb
+++ Formula/hurl.rb
@@ -1,9 +1,9 @@
 class Hurl < Formula
   desc "Run and Test HTTP Requests."
   homepage "https://hurl.dev"
-  url "https://github.com/Orange-OpenSource/hurl/releases/download/1.1.0/hurl-1.1.0-x86_64-osx.tar.gz"
-  sha256 "cbc9cef1ca50a6ee500c50bd001baaf6e236095400405fd6f3acacfa7a6419e4"
-
+  url "https://github.com/Orange-OpenSource/hurl/releases/download/1.2.0/hurl-1.2.0-x86_64-osx.tar.gz"
+  sha256 "0b483b5130dbf15fddeeb2e3a2cfcae1e1eb3ece5ac6b8622fcba28711733f94"
+  
   def install
     bin.install "hurl"
     bin.install "hurlfmt"
